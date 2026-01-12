f7adcf6732ac209b9e657f6dbd350755c13e3219
EbbGithub08 <ebbe.gaston.zelow@gmail.com>
2026-01-12 11:56:06 +0100

    Fikk Pygame til å fungere og lagde en basic pygame vindu

--- test1.py
+++ test1.py
@@ -1,2 +1,22 @@
-for i in range(5):
-    print("Hello World!")
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("My First Pygame")
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((30, 30, 30))  # background color
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
