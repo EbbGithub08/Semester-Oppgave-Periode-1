6b78f6533a275913f4135e3921ae3250bf4b31d4
EbbGithub08 <ebbe.gaston.zelow@gmail.com>
2026-01-14 15:25:49 +0100

    Halveis gjennom å bygge opp et system for å plasere sprites i verden gridden.

--- Tutorial_Platformer.py
+++ Tutorial_Platformer.py
@@ -6,11 +6,46 @@ pygame.init()
 screen_width = 1000
 screen_height = 1000
 
+tile_size = 200
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Platformer")
 
 sun_img = pygame.image.load('platformer_assets/img/sun.png')
 bg_img = pygame.image.load('platformer_assets/img/sky.png')
+
+
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+        
+        dirt_img = pygame.image.load('platformer_assets/img/dirt.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect =img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile =(img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+
+
+
+world_data = [
+[1,1,1,1,1],
+[1,0,0,0,1],
+[1,0,0,0,1],
+[1,0,0,0,1],
+[1,1,1,1,1],        
+]
 
 run = True
 while run == True:
